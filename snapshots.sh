05a18276e48152a03ba6ebd0c04c82a1ea73d6c5
Mathew Augustine <mathew.p.a@gmail.com>
2016-10-10 11:30:21 +1100

    shell script to invoke the python snapshot script

---
+++ snapshots.sh
@@ -0,0 +1,3 @@
+/usr/bin/python26 /root/bin/createsnap.py  -v vol-xxxxxxxxx  -t WIKI 	-s False
+/usr/bin/python26 /root/bin/createsnap.py  -v vol-xxxxxxxxx  -t VPN 	-s False
+/usr/bin/python26 /root/bin/createsnap.py  -v vol-xxxxxxxxx  -t TEST    -s False
